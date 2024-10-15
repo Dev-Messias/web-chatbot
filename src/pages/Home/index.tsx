f40544df4c31bfd7833710bcae85c1308dec1b4b
DevMessias <messiasbento944@gmail.com>
2024-10-15 11:00:44 -0300

    first commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,14 @@
+function Home() {
+
+
+    return (
+      <div className="w-full h-screen flex flex-col items-center justify-center" >
+        <div >
+          <h1 className="text-xl text-red-500" >Home</h1>
+          
+        </div>
+      </div>
+    )
+  }
+  
+  export default Home;
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,17 @@
+
+
+
+function Login() {
+
+
+  return (
+    <div className="w-full h-screen flex flex-col items-center justify-center" >
+      <div >
+        <h1 className="text-xl text-red-500" >Login</h1>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Login
